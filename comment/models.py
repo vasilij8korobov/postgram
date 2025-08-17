2127c2c9bb5bcbec4644ad567fb2174275462ed6
vasilij korobov <kavyabass51@gmail.com>
2025-08-17 15:46:20 +0700

    Создание моделей

--- comment/models.py
+++ comment/models.py
@@ -1,3 +1,40 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+from config import settings
+from post.models import Post
+
+
+class Comment(models.Model):
+    """
+    Модель комментария
+    """
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name='post_comments'
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        verbose_name=_("Автор")
+    )
+
+    text = models.TextField(verbose_name=_("Текст"))
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name=_("Дата создания")
+    )
+
+    updated_at = models.DateField(
+        auto_now=True,
+        verbose_name=_('Дата редактирования')
+    )
+
+    class Meta:
+        verbose_name = _("Comment")
+        verbose_name_plural = _("Comments")
+        ordering = ['created_at']
--- post/models.py
+++ post/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from config.DRY import NULLABLE
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    """
+    Модель поста
+    """
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Заголовок',
+        help_text=_('Максимум 255 символов')
+    )
+
+    text = models.TextField(verbose_name='Начните писать текст')
+
+    image = models.ImageField(
+        upload_to='images/posts/%Y/%m/%d/',
+        **NULLABLE,
+        verbose_name='Изображение'
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        verbose_name=_('Автор')
+    )
+
+    comment = models.ManyToManyField(  # Лучше отдельная модель для комментариев
+        'Comment',
+        related_name='post_comments',
+        verbose_name='Комментарий',
+        **NULLABLE
+    )
+
+    created_at = models.DateField(
+        auto_now_add=True,
+        verbose_name='Дата создания'
+    )
+
+    updated_at = models.DateField(
+        auto_now=True,
+        verbose_name='Дата редактирования'
+    )
+
+    def __str__(self):
+        return f"{self.author} | {self.title}"
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+        ordering = ['-created_at']
